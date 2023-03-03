0e04e888fb60facef2dc40a81cf56311ad1699f4
Bishnu Dev <61447680+bordernone@users.noreply.github.com>
2023-03-03 23:48:36 +0000

    added login, updated navbar

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,31 +1,53 @@
 import React from "react";
 
-function Navbar() {
+const authLinks = [
+    {
+        name: "Home",
+        path: "/",
+    },
+    {
+        name: "Chat",
+        path: "/messageList",
+    },
+    {
+        name: "Profile",
+        path: "/profile/1",
+    },
+];
+
+const unAuthLinks = [
+    {
+        name: "Login",
+        path: "/login",
+    },
+    {
+        name: "Sign Up",
+        path: "/signup",
+    },
+];
+
+function Navbar(props) {
+    const { user } = props;
+
+    const links = user ? authLinks : unAuthLinks;
+
     return (
         <>
             <nav className="navbar fixed-bottom navbar-expand navbar-light bg-light">
                 <div className="container-fluid">
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav d-flex justify-content-around w-100">
-                            <li className="nav-item">
-                                <a
-                                    className="nav-link active"
-                                    aria-current="page"
-                                    href="/"
-                                >
-                                    Home
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/messageList">
-                                    Chat
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/profile/1">
-                                    Profile
-                                </a>
-                            </li>
+                            {
+                                links.map((link, index) => { 
+                                    return (
+                                        <li className="nav-item" key={index}>
+                                            <a className={"nav-link " + ((window.location.pathname === link.path)? "active": "")} href={link.path} >
+                                                {link.name}
+                                            </a>
+                                        </li>
+                                    )
+                                 })
+                            }
                         </ul>
                     </div>
                 </div>
