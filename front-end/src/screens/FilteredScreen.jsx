c67fd79cc0dc29f3eb18e4ec2dccbf36e689d2f9
Bishnu Dev <61447680+bordernone@users.noreply.github.com>
2023-03-04 23:07:17 +0000

    added heading

--- front-end/src/screens/FilteredScreen.jsx
+++ front-end/src/screens/FilteredScreen.jsx
@@ -1,6 +1,39 @@
+import { Button } from "react-bootstrap";
 import React from "react";
+import Navbar from "../components/Navbar";
+import TitleBar from "../components/TitleBar";
+
+const SearchBtnWithFilter = () => (
+    <div className="row p-2 text-center mt-4">
+        <div className="col-4 p-1">
+            <Button variant="secondary" size="sm">
+                Search
+            </Button>
+        </div>
+        <div className="col-8 border p-1">CS, Agile..., March 12, Online</div>
+    </div>
+);
+
+const FilteredItem = () => (
+	<div className="row">
+
+	</div>
+);
+
 export default function FilteredScreen() {
     return (
-      <div>FilteredScreen</div>
+        <>
+            <TitleBar />
+
+            <div className="content-body">
+                <div className="container-fluid pageLayout">
+					<SearchBtnWithFilter />
+					<FilteredItem />
+					<FilteredItem />
+				</div>
+            </div>
+
+            <Navbar user="test" />
+        </>
     );
-  }
+}
