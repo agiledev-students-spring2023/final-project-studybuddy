569d17652905e2217d7fc23b980fde862e31ef76
Bishnu Dev <61447680+bordernone@users.noreply.github.com>
2023-03-04 23:36:07 +0000

    added filtered screen

--- front-end/src/screens/FilteredScreen.jsx
+++ front-end/src/screens/FilteredScreen.jsx
@@ -15,9 +15,23 @@ const SearchBtnWithFilter = () => (
 );
 
 const FilteredItem = () => (
-	<div className="row">
-
-	</div>
+    <div className="row border p-1 pt-2 pb-2 m-1">
+        <p className="mb-1">Major of the Post's Buddy</p>
+        <h5 className="mb-1">Subject Topic / Field</h5>
+        <p className="m-0">Meeting Date and Time</p>
+        <div className="row pt-2 pb-2">
+            <div className="col-6 text-center">
+                <button className="btn btm-md btn-primary">
+                    View Profile
+                </button>
+            </div>
+            <div className="col-6 text-center">
+                <button className="btn btn-md btn-secondary">
+                    Message
+                </button>
+            </div>
+        </div>
+    </div>
 );
 
 export default function FilteredScreen() {
@@ -27,10 +41,12 @@ export default function FilteredScreen() {
 
             <div className="content-body">
                 <div className="container-fluid pageLayout">
-					<SearchBtnWithFilter />
-					<FilteredItem />
-					<FilteredItem />
-				</div>
+                    <SearchBtnWithFilter />
+                    <FilteredItem />
+                    <FilteredItem />
+                    <FilteredItem />
+                    <FilteredItem />
+                </div>
             </div>
 
             <Navbar user="test" />
