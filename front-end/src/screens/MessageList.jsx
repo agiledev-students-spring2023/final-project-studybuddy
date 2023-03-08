754cec06d6918728346087968a453da33c732428
sywu430 <ys4598@nyu.edu>
2023-03-08 10:19:01 -0500

    changed chat list(made preview components)

--- front-end/src/screens/MessageList.jsx
+++ front-end/src/screens/MessageList.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import MessagePreview from "../components/MessagePreview";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import "./MessageList.css"
 
 export default function MessageList() {
+    let preview = "Hello, my name is yewon"
+    let name = "Yewon Song"
+    let img_url = "https://picsum.photos/50/50"
+
     return (
-      <div>
-        {/* <div>Header</div> */}
-        <div className="chat_list_body">
-          <MessagePreview preview={"Hello, my name is yewon"} name={"Yewon Song"} img={"https://picsum.photos/50/50"}/>
+      <div className="screen">
+        <div className="screen_header">
+          All User Messages with Other Buddies
         </div>
-        {/* <div>Navigation Bar</div> */}
+        <div className="screen_body">
+          <MessagePreview preview={preview} name={name} img={img_url} unread = {100}/>
+          <MessagePreview preview={preview} name={name} img={img_url} unread = {10}/>
+          <MessagePreview preview={preview} name={name} img={img_url} unread = {5}/>
+          <MessagePreview preview={preview} name={name} img={img_url} unread = {6}/>
+          <MessagePreview preview={preview} name={name} img={img_url} unread = {70}/>
+          <MessagePreview preview={preview} name={name} img={img_url} unread = {30}/>
+        </div>
+        <Navbar user="Others"/>
       </div>
     );
   }
