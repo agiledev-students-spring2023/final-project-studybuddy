023b9ceb8cd7258371511e9187ec6daa82172b17
Kelo2000 <ltl2113@nyu.edu>
2023-03-27 15:35:25 -0400

    Adding changes

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -4,8 +4,8 @@ const router = express.Router()
 const main = require('./main.js')
 const user = require('./user.js')
 const chat = require('./chat.js')
-const chatList = require('/chatList.js')
-const sendChat = require('/sendChat.js')
+const chatList = require('./chatList.js')
+const sendChat = require('./sendChat.js')
 
 //ADD routes here:
 router.use('/', main) 
