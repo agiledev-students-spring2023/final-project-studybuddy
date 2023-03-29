792d098de9736c3894de80a934e5a0aed8ba83f7
bordernone <devbishnu2000@gmail.com>
2023-03-29 22:04:27 +0000

    formatting

--- front-end/src/screens/Chat.jsx
+++ front-end/src/screens/Chat.jsx
@@ -7,87 +7,86 @@ import "./Chat.css";
 import axios from "axios";
 
 export default function Chat() {
-  const [name, setName] = useState("");
-  const [messages, setMessages] = useState([]);
-  const [input, setInput] = useState("");
+	const [name, setName] = useState("");
+	const [messages, setMessages] = useState([]);
+	const [input, setInput] = useState("");
 
-  const chatAPI = 'http://localhost:4000/chat/123';
+	const chatAPI = "http://localhost:4000/chat/123";
 
-  useEffect(() => {
-    // this function will be called just once.
-    async function fetchChatData() {
-      const { data } = await axios.get(chatAPI);
-      const { messages, name } = data
-      
-      setMessages(messages);
-      setName(name);
-    }
+	useEffect(() => {
+		// this function will be called just once.
+		async function fetchChatData() {
+			const { data } = await axios.get(chatAPI);
+			const { messages, name } = data;
 
-    fetchChatData();
+			setMessages(messages);
+			setName(name);
+		}
 
-  }, []);
+		fetchChatData();
+	}, []);
 
-  const sendMessage = () => {
-    if (input.length === 0) return; // empty input
+	const sendMessage = () => {
+		if (input.length === 0) return; // empty input
 
-    /* Message (isMe, content, timestamp) */
-    const msg = {
-      isMe: true,
-      content: input,
-      timestamp: Date.now(),
-    };
-    window.scrollTo(0, document.body.scrollHeight);
-    /* push new message */
-    const newMessages = messages.concat(msg);
-    setMessages(newMessages);
+		/* Message (isMe, content, timestamp) */
+		const msg = {
+			isMe: true,
+			content: input,
+			timestamp: Date.now(),
+		};
+		window.scrollTo(0, document.body.scrollHeight);
+		/* push new message */
+		const newMessages = messages.concat(msg);
+		setMessages(newMessages);
 
 		// TODO: send message to back-end
 
-    /* reset input */
-    setInput("");
+		/* reset input */
+		setInput("");
 
-    scrollToBottom();
-  };
+		scrollToBottom();
+	};
 
-  const scrollToBottom = () => {
-    const element = document.getElementById("chat_body");
-    element.scrollTop = element.scrollHeight;
-  };
+	const scrollToBottom = () => {
+		const element = document.getElementById("chat_body");
+		element.scrollTop = element.scrollHeight;
+	};
 
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter") sendMessage();
-  };
+	const handleKeyDown = (e) => {
+		if (e.key === "Enter") sendMessage();
+	};
 
-  const handleButtonClick = () => {
-    sendMessage();
-  };
+	const handleButtonClick = () => {
+		sendMessage();
+	};
 
-  return (
-    <div className="chat_screen">
-      <div className="chat_screen_header">
-        <MdArrowBack
-          className="cursor_pointer"
-          onClick={() => window.history.back()}
-        />
-        {name}{" "}
-      </div>
-      <div className="chat_screen_body" id="chat_body">
-        {messages.map((e, i) => (
-          <ChatBubble key={i} chat={e} />
-        ))}
-      </div>
-      <div className="chat_input_container">
-        <Form.Control
-          type="text"
-          value={input}
-          placeholder="Type message"
-          onChange={(e) => setInput(e.target.value)}
-          onKeyDown={handleKeyDown}
-        />
-        <button className="btn_chatsend" onClick={handleButtonClick}>
-          <MdSend />
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="chat_screen">
+			<div className="chat_screen_header">
+				<MdArrowBack
+					className="cursor_pointer"
+					onClick={() => window.history.back()}
+				/>
+				{name}{" "}
+			</div>
+			<div className="chat_screen_body" id="chat_body">
+				{messages.map((e, i) => (
+					<ChatBubble key={i} chat={e} />
+				))}
+			</div>
+			<div className="chat_input_container">
+				<Form.Control
+					type="text"
+					value={input}
+					placeholder="Type message"
+					onChange={(e) => setInput(e.target.value)}
+					onKeyDown={handleKeyDown}
+				/>
+				<button className="btn_chatsend" onClick={handleButtonClick}>
+					<MdSend />
+				</button>
+			</div>
+		</div>
+	);
 }
--- front-end/src/screens/Home.jsx
+++ front-end/src/screens/Home.jsx
@@ -4,29 +4,41 @@ import { MdChatBubble, MdPerson, MdSearch, MdUpload } from "react-icons/md";
 import "./Home.css";
 
 export default function Home() {
-  return (
-    <div className="screen">
-      <div className="screen_header">Study Buddy</div>
-      <div className="screen_body home_container">
-        <div className="home_search_btn" onClick={() => window.location.href = '/filters'}>
-          <div className="search_text">Search Post</div>
-          <MdSearch />
-        </div>
-        <div className="home_btn_container">
-          <div className="home_profile_btn" onClick={() => window.location.href = '/profile'}>
-            Profile
-            <MdPerson className="home_icon" />
-          </div>
-          <div className="home_chat_btn" onClick={() => window.location.href = '/chatList'}>
-            Chats
-            <MdChatBubble className="home_icon" />
-          </div>
-          <div className="home_upload_btn" onClick={() => window.location.href = '/uploadPost'}>
-            Upload Post
-            <MdUpload className="home_icon" />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="screen">
+			<div className="screen_header">Study Buddy</div>
+			<div className="screen_body home_container">
+				<div
+					className="home_search_btn"
+					onClick={() => (window.location.href = "/filters")}
+				>
+					<div className="search_text">Search Post</div>
+					<MdSearch />
+				</div>
+				<div className="home_btn_container">
+					<div
+						className="home_profile_btn"
+						onClick={() => (window.location.href = "/profile")}
+					>
+						Profile
+						<MdPerson className="home_icon" />
+					</div>
+					<div
+						className="home_chat_btn"
+						onClick={() => (window.location.href = "/chatList")}
+					>
+						Chats
+						<MdChatBubble className="home_icon" />
+					</div>
+					<div
+						className="home_upload_btn"
+						onClick={() => (window.location.href = "/uploadPost")}
+					>
+						Upload Post
+						<MdUpload className="home_icon" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
