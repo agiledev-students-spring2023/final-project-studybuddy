39d74586a9c01fa85967a96fafde363358eb4f9a
Kelo2000 <ltl2113@nyu.edu>
2023-04-02 18:01:52 -0400

    Updating Viewpost front-end

--- front-end/src/screens/ViewPost.jsx
+++ front-end/src/screens/ViewPost.jsx
@@ -3,7 +3,6 @@ import Navbar from "../components/Navbar";
 import TitleBar from "../components/TitleBar";
 import "./ViewPost.css";
 import axios from "axios";
-import { format } from "date-fns/fp";
 import { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import { MdSend } from "react-icons/md";
@@ -30,7 +29,8 @@ const Item = ({ title, date_time }) => {
 const ViewPost = () => {
 	const [posts, setPosts] = useState([]);
 	const [input, setInput] = useState("");
-	const { profile, id } = useParams();
+	const {postId}=useParams();
+	const message_url = `/chat/${postId}`
 
 	const handleKeyDown = (e) => {
 		if (e.key === "Enter") setInput("");
@@ -41,10 +41,11 @@ const ViewPost = () => {
 	};
 
 	useEffect(() => {
-		loadFilteredPosts();
-	}, []);
+		loadFilteredPosts(postId);
+	}, [postId]);
 
-	const loadFilteredPosts = () => {
+	const loadFilteredPosts = (postId) => {
+		console.log(postId);
 		const options = {
 			method: "GET",
 			url: "https://my.api.mockaroo.com/posts.json",
@@ -58,8 +59,9 @@ const ViewPost = () => {
 			.request(options)
 			.then(function (response) {
 				console.log(response.data);
-				const object = response.data.find((obj) => obj.id === 2);
+				const object = response.data.find((obj) => obj.id == postId);
 				setPosts(object);
+				
 			})
 			.catch(function (error) {
 				console.error(error);
@@ -68,32 +70,45 @@ const ViewPost = () => {
 
 	let back;
 
-	if (posts.id === 1) {
-		back = "/profile";
-	} else {
-		back = "/userprofile";
-	}
+	// if (posts.id === 1) {
+	// 	back = "/profile";
+	// } else {
+	// 	back = "/userprofile";
+	// }
 
 	return (
 		<div className="View Post">
 			<TitleBar title="View Post" backpage={back} />
 			<div className="content-body">
 				<div className="container-fluid pageLayout">
-					<div className="profile">
-						<div className="mb-1">
-							<img
-								src={posts.profile_pic}
-								className="Picture"
-								alt="ProfilePicture"
-							/>
+					<div className="Buddy">
+						<div className="profile">
+							<div className="mb-1">
+								<img
+									src={posts.profile_pic}
+									className="Picture"
+									alt="ProfilePicture"
+								/>
+							</div>
+							<div className="mydetails">
+								<h2 className="mb-1">{posts.name}</h2>
+								<h2 className="mb-1">{posts.major}</h2>
+							</div>
 						</div>
-						<div className="mydetails">
-							<h2 className="mb-1">{posts.name}</h2>
-							<h2 className="mb-1">{posts.major}</h2>
+						<div className="MessageBuddy">
+							<a
+								href={message_url}
+								style={{
+									textDecoration: "none",
+								}}
+							>
+								Direct Message
+							</a>
 						</div>
 					</div>
-					<Item title={posts.title} date_time={posts.date_time} />
 
+					<Item title={posts.title} date_time={posts.date_time} />
+					
 					<div className="Description">
 						<p>{posts.description}</p>
 					</div>
