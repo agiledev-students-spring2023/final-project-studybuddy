6b2e5473ad20b291d7602cd7d156859d6ff04be4
bordernone <devbishnu2000@gmail.com>
2023-04-15 21:52:48 +0000

    connect sign up to db

--- back-end/controllers/auth.controller.js
+++ back-end/controllers/auth.controller.js
@@ -1,7 +1,12 @@
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
 const {
 	sendResetPasswordEmail,
 	generateResetToken,
 } = require("../utilities/auth.utils");
+const UserModel = require("../models/user.model");
+
+const SALT_ROUNDS = 10;
 
 const loginController = (req, res) => {
 	const { username, password } = req.body;
@@ -40,8 +45,34 @@ const resetPasswordController = (req, res) => {
 	return res.status(200).json({ message: "Password reset successfully" });
 };
 
-const registerController = (req, res) => {
-	return res.status(200).json({ message: "Register successfully" });
+const registerController = async (req, res) => {
+	const { name, major, username, email, password } = req.body;
+	try {
+		// Check if the username and email already exists
+		if (await UserModel.exists({ username })) {
+			return res.status(400).json({ message: "Username already exists" });
+		}
+		if (await UserModel.exists({ email })) {
+			return res.status(400).json({ message: "Email already exists" });
+		}
+
+		// Save the user to the database
+		const salt = await bcrypt.genSalt(SALT_ROUNDS);
+		const hashedPassword = await bcrypt.hash(password, salt);
+		const newUser = new UserModel({
+			_id: new mongoose.Types.ObjectId(),
+			name,
+			major,
+			username,
+			email,
+			password: hashedPassword,
+		});
+		await newUser.save();
+		return res.status(200).json({ message: "User created successfully" });
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ message: error.message });
+	}
 };
 
 module.exports = {
