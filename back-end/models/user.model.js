5363df0f799671bb4a9d209ed4b7870cae1a8bda
Kelo2000 <ltl2113@nyu.edu>
2023-04-14 20:32:22 -0400

    User model for Database

--- back-end/models/user.model.js
+++ back-end/models/user.model.js
@@ -1,16 +1,15 @@
 const { default: mongoose } = require("mongoose");
-const Schema=mongoose.Schema;
-const ObjectId=Schema.ObjectId;
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
 
-const UserSchema=new Schema({
-    _id:ObjectId,
-    name: {type: String, required: true},
-    major: {type: String, required: true},
-    username:{type: String, required: true, unique: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    Profile_pic: {data: Buffer, contentType: String},
-
+const UserSchema = new Schema({
+	_id: ObjectId,
+	name: { type: String, required: true },
+	major: { type: String, required: true },
+	username: { type: String, required: true, unique: true },
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	Profile_pic: { data: Buffer, contentType: String },
 });
 
-module.exports=mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", UserSchema);
