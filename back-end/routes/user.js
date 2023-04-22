aeaa971bbed5a0c6a2e340e54218776b958f774d
Kelo2000 <ltl2113@nyu.edu>
2023-04-21 23:00:25 -0400

    Final integration database and profiles

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -2,29 +2,26 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const User = require("../models/user.model");
+const { PostModel } = require("../models/post.model");
 const mongoose = require("mongoose");
 
 router.get("/:userId", async (req, res) => {
-	try {
-		const userId = req.params.userId;
-		const tempUserId = "643b1b8926271cb644835017";
-		const url = "https://my.api.mockaroo.com/study_buddy_data.json";
-		const key = "a015ead0";
+    try {
+        const userId = req.params.userId;
+        const tempUserId = "643b1b8926271cb644835017";
+        const url = "https://my.api.mockaroo.com/study_buddy_data.json";
+        const key = "a015ead0";
 
-		const user = await User.findById(tempUserId);
-		const response = await axios.get(url, {
-			params: { key },
-			headers: {
-				cookie: "layer0_bucket=90; layer0_destination=default; layer0_environment_id_info=1680b086-a116-4dc7-a17d-9e6fdbb9f6d9",
-			},
-		});
-		const posts = response.data;
-		const filteredData = posts.find((item) => item.id === parseInt(userId));
-		return res.json({ filteredData, user });
-	} catch (error) {
-		console.log(error);
-		return res.status(500).send("Error retrieving data from mockAPI");
-	}
+        const user = await User.findById(tempUserId);
+        const postID = user.posts;
+
+        const posts = await PostModel.find({ _id: { $in: postID } });
+
+        return res.json({ posts, user });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send("Error retrieving data from mockAPI");
+    }
 });
 
 module.exports = router;
