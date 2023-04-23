fc993fc6e388145eb84c05b4d157ac55e2c7bc26
sywu430 <ys4598@nyu.edu>
2023-04-23 22:05:06 +0900

    add chat/chatList error screen and chat.utils

--- front-end/src/screens/ChatList.jsx
+++ front-end/src/screens/ChatList.jsx
@@ -4,23 +4,42 @@ import MessagePreview from "../components/MessagePreview";
 import Navbar from "../components/Navbar";
 import "./ChatList.css";
 import { getToken } from "../auth/auth"
+import ChatError from "../components/ChatError";
+import Loader from "../components/Loader";
 
 
 export default function ChatList() {
 	const [chatList, setChatList] = useState([]);
+	const [success, setSuccess] = useState(true)
+	const [idx, setIdx] = useState(1)
+	const [loading, setLoading] = useState(false)
 
 	useEffect(() => {
 		async function fetchChatListData() {
-			const options = {
-				method: "GET",
-				url: "/_chat",
-				headers: {
-					authorization: getToken(),
-				},
-			};
-			const { data: { chatlist } } = await axios.request(options)
+			setLoading(true)
+			try {
+				const options = {
+					method: "GET",
+					url: "/_chat",
+					headers: {
+						authorization: getToken(),
+					},
+				};
+				const { data: { chatlist } } = await axios.request(options)
 
-			setChatList(chatlist);
+				setChatList(chatlist);
+				if (chatlist.length) {
+					setSuccess(true)
+				} else {
+					setSuccess(false)
+					setIdx(1)
+				}
+			} catch {
+				setChatList([]);
+				setSuccess(false)
+				setIdx(0)
+			}
+			setLoading(false)
 		}
 
 		fetchChatListData();
@@ -28,12 +47,13 @@ export default function ChatList() {
 
 	return (
 		<div className="screen">
-			<div className="screen_header">Chats</div>
-			<div className="screen_body">
-				{chatList.map((e, i) => (
-					<MessagePreview key={i} chat={e} />
-				))}
-			</div>
+			{loading ? <Loader /> : success ? <><div className="screen_header">Chats</div>
+				<div className="screen_body">
+					{chatList.map((e, i) => (
+						<MessagePreview key={i} chat={e} />
+					))}
+				</div>
+			</> : <ChatError idx={idx} />}
 			<Navbar user="Others" />
 		</div>
 	);
