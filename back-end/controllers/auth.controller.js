463832bb3850b7e65480da1b18993a4730ceb818
bordernone <devbishnu2000@gmail.com>
2023-05-03 02:08:26 -0400

    add templates for email confirmation and password reset

--- back-end/controllers/auth.controller.js
+++ back-end/controllers/auth.controller.js
@@ -4,9 +4,11 @@ const jwt = require("jsonwebtoken");
 const {
 	sendResetPasswordEmail,
 	generateResetToken,
+	generateConfirmationToken,
+	sendVerificationEmail,
 } = require("../utilities/auth.utils");
 const UserModel = require("../models/user.model");
-const { ResetPassword } = require("../models/auth.model");
+const { ResetPassword, EmailConfirmation } = require("../models/auth.model");
 
 const SALT_ROUNDS = 10;
 
@@ -19,6 +21,12 @@ const loginController = async (req, res) => {
 	const isPasswordCorrect = await bcrypt.compare(password, user.password);
 	if (!isPasswordCorrect)
 		return res.status(400).json({ message: "Invalid password" });
+
+	if (!(await EmailConfirmation.exists({ userid: user._id, confirmed: true }))) {
+		return res.status(400).json({
+			message: "Please verify your email before logging in",
+		});
+	}
 
 	const token = jwt.sign(
 		{ username: user.username, email: user.email, id: user._id },
@@ -79,7 +87,7 @@ const forgotPasswordController = async (req, res) => {
 const resetPasswordController = async (req, res) => {
 	const { password, token, username } = req.body;
 
-	// TODO: Check if the token is valid
+	// Check if the token is valid
 	const resetPassword = await ResetPassword.findOne({
 		username,
 		reset_token: token,
@@ -89,13 +97,27 @@ const resetPasswordController = async (req, res) => {
 	if (resetPassword.reset_token_expiration < Date.now())
 		return res.status(400).json({ message: "Reset token has expired" });
 
-	// TODO: Update the password in the database
+	// Update the password in the database
 	try {
 		const salt = await bcrypt.genSalt(SALT_ROUNDS);
 		const hashedPassword = await bcrypt.hash(password, salt);
 		const user = await UserModel.findOne({ username });
 		user.password = hashedPassword;
 		await user.save();
+
+		let emailConfirmation = await EmailConfirmation.findOne({ userid: user._id });
+		if (!emailConfirmation) {
+			emailConfirmation = new EmailConfirmation({
+				_id: new mongoose.Types.ObjectId(),
+				userid: user._id,
+				confirmation_token: token,
+				confirmed: true,
+			});
+			await emailConfirmation.save();
+		} else {
+			emailConfirmation.confirmed = true;
+			await emailConfirmation.save();
+		}
 
 		// Delete the reset token from the database
 		await ResetPassword.findByIdAndDelete(resetPassword._id);
@@ -130,10 +152,54 @@ const registerController = async (req, res) => {
 			password: hashedPassword,
 		});
 		await newUser.save();
-		return res.status(200).json({ message: "User created successfully" });
+
+		// Send Email Confirmation Email
+		const token = generateConfirmationToken(email, username);
+
+		const newEmailConfirmation = new EmailConfirmation({
+			_id: new mongoose.Types.ObjectId(),
+			userid: newUser._id,
+			confirmation_token: token,
+			confirmed: false,
+		});
+
+		await newEmailConfirmation.save();
+
+		let success = await sendVerificationEmail(email, token, username);
+		if (!success) {
+			console.error("Failed to send verification email");
+			return res.status(500).json({ message: "Something went wrong" });
+		}
+
+		return res.status(200).json({
+			message: "Please check your email to confirm your account",
+		});
 	} catch (error) {
 		console.error(error);
 		return res.status(500).json({ message: error.message });
+	}
+};
+
+const verifyEmailController = async (req, res) => {
+	const { token, username } = req.query;
+
+	// Check if the token is valid
+	if (!(await EmailConfirmation.exists({ username, token }))) {
+		return res.status(400).json({ message: "User doesn't exist" });
+	}
+
+	// Update the status of the user
+	try {
+		const emailConfirmation = await EmailConfirmation.findOne({
+			username,
+			token,
+		});
+		emailConfirmation.confirmed = true;
+		await emailConfirmation.save();
+		return res.redirect(`${process.env.DOMAIN}/Login`);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ message: "Something went wrong" });
 	}
 };
 
@@ -142,4 +208,5 @@ module.exports = {
 	forgotPasswordController,
 	resetPasswordController,
 	registerController,
+	verifyEmailController,
 };
